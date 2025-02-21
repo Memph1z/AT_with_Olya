3161ac7855574b8e2b5e69f9dafbdd019c8090e4
sousFlex <memph1zgray@gmail.com>
2025-02-21 18:58:58 +0300

    Написали новые классы, функции и методы

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,13 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Square square = new Square(4);
+        Square secondSquare = new Square(8);
+        System.out.println("Hello yopta");
+        System.out.println("Площадь квадрата со стороной " + square.l + " = " + square.area());
+        System.out.println("Площадь квадрата со стороной " + secondSquare.l + " = " + secondSquare.area());
     }
+
 }
