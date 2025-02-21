e4dc36067c16ca05b558f83a08192ccb6380fc14
sousFlex <memph1zgray@gmail.com>
2025-02-21 19:03:52 +0300

    Дописан класс Rectangle, переписан метод area

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,9 +5,11 @@ public class MyFirstProgram {
     public static void main(String[] args) {
         Square square = new Square(4);
         Square secondSquare = new Square(8);
+        Rectangle rectangle = new Rectangle(5, 4);
         System.out.println("Hello yopta");
         System.out.println("Площадь квадрата со стороной " + square.l + " = " + square.area());
         System.out.println("Площадь квадрата со стороной " + secondSquare.l + " = " + secondSquare.area());
+        System.out.println("Площадь прямоугольника со сторонами " + rectangle.a + " и " + rectangle.b + " = " + rectangle.area());
     }
 
 }
